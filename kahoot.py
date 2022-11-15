662eafd7f65d23761a91e25a4dc9d7727bf47c4c
Wambuingare <wambuinancyngare@gmail.com>
2022-11-15 17:59:46 +0300

    Operators

---
+++ kahoot.py
@@ -0,0 +1,26 @@
+x={"jodi","eric","garry"}
+x.add(1,"vicky")
+print(x)
+
+for x in range(0.5,5.5,0.5):
+    print(x)
+
+
+def calc(num1,num2):
+    res=num1*num2
+    print(res)
+
+calc(5,6) 
+   
+x =36/4*(3+2)*4+2
+print(x)
+
+var1=1
+var2=2
+var3="3"
+print(var1*var2*var3)
+
+x=[1,2,3,4]
+y=[1,2,3,4]
+print(x==y)
+print(x is y)
